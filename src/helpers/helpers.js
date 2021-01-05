769d4f1af2346cc5555290fd72a6778b7e0cf3f9
AyloSrd <aylo.srd@gmail.com>
2021-01-05 21:25:43 +0100

    in comments, started a way to pixelate parts only using getImageData and putImageData, as of now it only inverts colors

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -15,16 +15,24 @@ export const div2Canvas = (width, height, bkImg, faceBoxes, test) => {
 	const ctx = canvas.getContext('2d');
 	ctx.drawImage(bkImg, 0, 0, canvas.width, canvas.width * bkImg.height / bkImg.width)
 	//draw smilies
-	faceBoxes.forEach(d => {
+	faceBoxes.forEach((d, i) => {
 		const {relX, relY, relW, relH} = d
 		// ctx.fillRect(relX*width, relY*height, relW*width, relH*height)
 		const imgSmile = document.createElement('img')
 		imgSmile.src = './smiley.png'
 		imgSmile.width = 20;
 		imgSmile.height = 20;
-		imgSmile.onload = function() {
+		imgSmile.onload = () => {
 		ctx.drawImage(imgSmile, relX*width, relY*height, relW*width, relH*height)
 		}
+		// const imgData = ctx.getImageData(relX*width, relY*height, relW*width, relH*height)
+		// const data = imgData.data
+		// for (let i = 0; i < data.length; i += 4) {
+		// 	data[i]     = 255 - data[i]     // rouge
+		// 	data[i + 1] = 255 - data[i + 1] // vert
+		// 	data[i + 2] = 255 - data[i + 2] // bleu
+		// 	}
+		// ctx.putImageData(imgData, relX*width, relY*height)
 	})
 	//append canvas
 	test.appendChild(canvas)
