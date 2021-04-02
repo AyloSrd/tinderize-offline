ed40e718dbdaedf91c1901b02755d75c1d096269
AyloSrd <aylo.srd@gmail.com>
2021-04-02 18:54:27 +0200

    new tentative

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,11 @@
-const CACHE_STATIC_NAME = 'static-v1.1-BETA';
+var CACHE_STATIC_NAME = 'static-v1.3-BETA';
 
-self.addEventListener('install', event => {
+self.addEventListener('install', function (event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
     caches.open(CACHE_STATIC_NAME)
-      .then(cache => {
-        console.log('[Service Worker] Precaching App Shell')
+      .then(function (cache) {
+        console.log('[Service Worker] Precaching App Shell');
         cache.addAll([
           '/',
           '/index.html',
@@ -25,20 +25,20 @@ self.addEventListener('install', event => {
         ])
       })
   )
-})
+});
 
-self.addEventListener('activate', event => {
-  console.log('[Service Worker] Activating Service Worker ....', event)
+self.addEventListener('activate', function (event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
   event.waitUntil(
     caches.keys()
-      .then( keyList => {
-        return Promise.all(keyList.map( key => {
+      .then(function (keyList) {
+        return Promise.all(keyList.map(function (key) {
           if (key !== CACHE_STATIC_NAME) {
-            console.log('[Service Worker] Removing old cache.', key)
-            return caches.delete(key)
+            console.log('[Service Worker] Removing old cache.', key);
+            return caches.delete(key);
           }
-        }))
+        }));
       })
-  )
-  return self.clients.claim()
-})
+  );
+  return self.clients.claim();
+});
