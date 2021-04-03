533d05bbe5d73f7d3b373efe1ec19a529339814e
AyloSrd <aylo.srd@gmail.com>
2021-04-03 11:14:49 +0200

    added activate evt listener to sw, removing olg cahs, test

--- public/sw.js
+++ public/sw.js
@@ -13,19 +13,35 @@ const ASSETS  = [
     'https://tinderize-offline.vercel.app/manifest.json'
 ]
 
-let cache_name = "tindoff-v3.0-beta";
+let CACHE_NAME = "tindoff-v4.0-beta";
 
 self.addEventListener("install", event => {
-    console.log("installing...");
+    console.log("installing...")
     event.waitUntil(
         caches
-            .open(cache_name)
+            .open(CACHE_NAME)
             .then(cache => {
                 return cache.addAll(ASSETS);
             })
             .catch(err => console.log(err))
-    );
-});
+    )
+})
+
+self.addEventListener('activate', event => {
+  console.log('[Service Worker] Activating Service Worker ....', event)
+  event.waitUntil(
+    caches.keys()
+      .then( keyList => {
+        return Promise.all(keyList.map(key => {
+          if (key.includes('tindoff') && key !== CACHE_NAME) {
+            console.log('[Service Worker] Removing old cache.', key)
+            return caches.delete(key)
+          }
+        }))
+      })
+  )
+  return self.clients.claim();
+})
 
 self.addEventListener("fetch", event => {
     event.respondWith(
@@ -34,68 +50,3 @@ self.addEventListener("fetch", event => {
         )
     )
 })
-
-
-// var CACHE_STATIC_NAME = 'static-v1.6-BETA';
-
-// self.addEventListener('install', function (event) {
-//   console.log('[Service Worker] Installing Service Worker ...', event);
-//   event.waitUntil(
-//     caches.open(CACHE_STATIC_NAME)
-//       .then(function (cache) {
-//         console.log('[Service Worker] Precaching App Shell');
-//         cache.addAll([
-//           '/',
-//           '/index.html',
-//           '/offline.html',
-//           '/global.css',
-//           '/manifest.json',
-//           '/face-api.min.js',
-//           '/favicon.ico',
-//           '/favicon.png',
-//           '/logo.png',
-//           '/smiley.png',
-//           '/models/tiny_face_detector_model-shard1',
-//           '/models/tiny_face_detector_model-weights_manifest.json',
-//           '/build/bundle.css',
-//           '/build/bundle.js',
-//           '/build/bundle.js.map',
-//           'https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,700;1,300;1,400;1,500;1,700&display=swap',
-//           'https://tinderize-offline.vercel.app/'
-//         ])
-//       })
-//   )
-// });
-
-// self.addEventListener('activate', function (event) {
-//   console.log('[Service Worker] Activating Service Worker ....', event);
-//   event.waitUntil(
-//     caches.keys()
-//       .then(function (keyList) {
-//         return Promise.all(keyList.map(function (key) {
-//           if (key !== CACHE_STATIC_NAME) {
-//             console.log('[Service Worker] Removing old cache.', key);
-//             return caches.delete(key);
-//           }
-//         }));
-//       })
-//   );
-//   return self.clients.claim();
-// });
-
-// self.addEventListener("fetch", event => {
-//     console.log("You fetched " + event.url);
-// });
-
-// self.addEventListener('fetch', function(event) {
-//     event.respondWith(
-//       caches.open(CACHE_STATIC_NAME)
-//         .then(function(cache) {
-//           return fetch(event.request)
-//             .then(function(res) {
-//               cache.put(event.request, res.clone());
-//               return res;
-//             });
-//         })
-//     );
-//   });
